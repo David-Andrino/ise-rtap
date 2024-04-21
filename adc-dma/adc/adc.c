f6e3c70e62f4996e3ff5bf00a7ca246bcd06f4cf
David-Andrino <davidandrino2002@gmail.com>
2024-04-21 19:24:59 +0200

    Move signal generation out of adc file

--- adc-dma/adc/adc.c
+++ adc-dma/adc/adc.c
@@ -1,22 +1,14 @@
 #include "../main.h"
 #include "adc.h"
 
-#define ADC_GPIO GPIOA
-#define ADC_PIN  GPIO_PIN_3
-
-#define TMP_TIM_GPIO GPIOD
-#define TMP_TIM_PIN  GPIO_PIN_12
-
 static ADC_HandleTypeDef hadc    = { 0 };
 static TIM_HandleTypeDef htim    = { 0 };
-static TIM_HandleTypeDef htmptim = { 0 };
 static DMA_HandleTypeDef hdma    = { 0 };
 
 static int convs = 0, value = 0;
 
 static int adc_init(void);
 static int adc_tim_init(void);
-static int tmp_tim_init(void);
 static int adc_dma_init(void);
 
 static sampling_Callback halfCb = NULL;
@@ -32,8 +24,7 @@ int sampling_init(sampling_Callback firstHalfCb, sampling_Callback secondHalfCb)
     if (secondHalfCb != NULL)
         fullCb = secondHalfCb;
     
-    return tmp_tim_init() | 
-           adc_tim_init() | 
+    return adc_tim_init() | 
            adc_init()     | 
            adc_dma_init() ;
 }
@@ -70,41 +61,6 @@ static int adc_init(void) {
         .SamplingTime = ADC_SAMPLETIME_56CYCLES
     };
     if (HAL_ADC_ConfigChannel(&hadc, &sConfig)) {
-        return -1;
-    }
-    
-    return 0;
-}
-
-static int tmp_tim_init(void) {
-    __HAL_RCC_GPIOD_CLK_ENABLE();
-    __HAL_RCC_TIM4_CLK_ENABLE();
-    GPIO_InitTypeDef tmpTimGPIO = {
-        .Pin = TMP_TIM_PIN,
-        .Mode = GPIO_MODE_AF_PP,
-        .Alternate = GPIO_AF2_TIM4,
-        .Pull = GPIO_NOPULL
-    };
-    HAL_GPIO_Init(TMP_TIM_GPIO, &tmpTimGPIO);
-    
-    htmptim.Instance = TIM4;
-    htmptim.Init.Prescaler = 999;
-    htmptim.Init.Period = 20;
-    if (HAL_TIM_OC_Init(&htmptim)) {
-        return -1;
-    }
-    
-    TIM_OC_InitTypeDef tmpTimChannel = {
-        .OCMode = TIM_OCMODE_TOGGLE,
-        .OCPolarity = TIM_OCPOLARITY_HIGH,
-        .OCFastMode = TIM_OCFAST_DISABLE
-    };
-    
-    if (HAL_TIM_OC_ConfigChannel(&htmptim, &tmpTimChannel, TIM_CHANNEL_1)) {
-        return -1;
-    }        
-
-    if (HAL_TIM_OC_Start(&htmptim, TIM_CHANNEL_1)) {
         return -1;
     }
     
--- adc-dma/adc/test-adc.c
+++ adc-dma/adc/test-adc.c
@@ -9,9 +9,12 @@ static int done = 0;
 static uint16_t  buffer[2048];
 static uint16_t* half_buf = &buffer[1024];
 
-static void samplingFullCb();
+static void samplingFullCb(void);
+static int tmp_tim_init(void);
+static TIM_HandleTypeDef htmptim = { 0 };
 
 void test_adc() {
+  tmp_tim_init();
   int ret = sampling_init(NULL, samplingFullCb);
   sampling_start(buffer, 2048);
   
@@ -30,4 +33,40 @@ void test_adc() {
 void samplingFullCb() {
     done = 1;
     sampling_stop();
+}
+
+
+static int tmp_tim_init(void) {
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_TIM4_CLK_ENABLE();
+    GPIO_InitTypeDef tmpTimGPIO = {
+        .Pin = GPIO_PIN_12,
+        .Mode = GPIO_MODE_AF_PP,
+        .Alternate = GPIO_AF2_TIM4,
+        .Pull = GPIO_NOPULL
+    };
+    HAL_GPIO_Init(GPIOD, &tmpTimGPIO);
+    
+    htmptim.Instance = TIM4;
+    htmptim.Init.Prescaler = 999;
+    htmptim.Init.Period = 20;
+    if (HAL_TIM_OC_Init(&htmptim)) {
+        return -1;
+    }
+    
+    TIM_OC_InitTypeDef tmpTimChannel = {
+        .OCMode = TIM_OCMODE_TOGGLE,
+        .OCPolarity = TIM_OCPOLARITY_HIGH,
+        .OCFastMode = TIM_OCFAST_DISABLE
+    };
+    
+    if (HAL_TIM_OC_ConfigChannel(&htmptim, &tmpTimChannel, TIM_CHANNEL_1)) {
+        return -1;
+    }        
+
+    if (HAL_TIM_OC_Start(&htmptim, TIM_CHANNEL_1)) {
+        return -1;
+    }
+    
+    return 0;
 }
