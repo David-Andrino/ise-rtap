617a065632b7de9fd89d2340d11400103edc22a0
David-Andrino <davidandrino2002@gmail.com>
2024-05-03 18:05:00 +0200

    Limpiar proyecto raiz

--- RadioAdaptada/Objects/radio.d
+++
@@ -1,7 +0,0 @@
-./objects/radio.o: radio.c RTE\_Target_1\Pre_Include_Global.h \
-  C:\Users\X421\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\RTOS2\Include\cmsis_os2.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h radio.h \
-  C:\Users\X421\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Driver\Include\Driver_I2C.h \
-  C:\Users\X421\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Driver\Include\Driver_Common.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h
--- dac-F7/dac/test-dac.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_DAC_H
-#define TEST_DAC_H
-
-extern void test_dac(void);
-
-#endif
