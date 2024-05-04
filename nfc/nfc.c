da7b04bbbcf4e2ca92f203e6b575f51267e1f63a
Estela Mora <estela.mora.barba@alumnos.upm.es>
2024-05-04 16:29:32 +0200

    Disable NoPull

--- nfc/nfc.c
+++ nfc/nfc.c
@@ -102,7 +102,7 @@ void GPIO_Init(void){
 	
 	GPIO_InitStruct.Pin = GPIO_PIN_11;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
 	
 	HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 }
